bf9fde9b55b44093d62b73dd9455b79d5b11959c
shahadatrucse <shahadat.rucse@gmail.com>
2023-12-06 01:50:56 +0600

    Add project files.

---
+++ MyStore/Pages/Client/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyStore.Pages.Client
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
